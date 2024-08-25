4488cca643a1e0d8f5492f6f883a45b28116d1cb
MecDev2 <wandaclementina418@gmail.com>
2024-08-25 18:31:35 -0300

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,20 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:8080'], // Permitir requisições do frontend
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
